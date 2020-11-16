541968d11d377a1084df6797011509984ceb1ce1
Steve Min <stevekmin@gmail.com>
2020-11-16 08:42:06 -0800

    complete the stick figure

--- src/components/Figure.js
+++ src/components/Figure.js
@@ -11,6 +11,14 @@ const Figure = () => {
         <line x1='20' y1='230' x2='100' y2='230' />
         {/* Head */}
         <circle cx='140' cy='70' r='20' class='figure-part' />
+        {/* Body */}
+        <line x1='140' y1='90' x2='140' y2='150' class='figure-part' />
+        {/* Arms */}
+        <line x1='140' y1='120' x2='120' y2='100' />
+        <line x1='140' y1='120' x2='160' y2='100' />
+        {/* Legs */}
+        <line x1='140' y1='150' x2='120' y2='180' />
+        <line x1='140' y1='150' x2='160' y2='180' />
       </svg>
     </div>
   );
