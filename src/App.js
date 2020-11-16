3bf996ea7e14259c4b7920f64b44a46da9cf6214
Steve Min <stevekmin@gmail.com>
2020-11-16 08:34:44 -0800

    add Figure component to DOM add circle for the head

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <React.Fragment>
       <Header />
+      <Figure />
     </React.Fragment>
   );
 }
--- src/components/Figure.js
+++ src/components/Figure.js
@@ -3,12 +3,14 @@ import React from 'react';
 const Figure = () => {
   return (
     <div>
-      {/* Rod */}
       <svg height='250' width='200' class='figure-container'>
+        {/* Rod */}
         <line x1='60' y1='20' x2='140' y2='20' />
         <line x1='140' y1='20' x2='140' y2='50' />
         <line x1='60' y1='20' x2='60' y2='230' />
         <line x1='20' y1='230' x2='100' y2='230' />
+        {/* Head */}
+        <circle cx='140' cy='70' r='20' class='figure-part' />
       </svg>
     </div>
   );
