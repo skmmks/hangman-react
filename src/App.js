a09a7e450b2a49e94d62b6dba76789f762e89dd6
Steve Min <stevekmin@gmail.com>
2020-11-18 07:44:10 -0800

    import and add wrong letter to app component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from './components/Header';
 import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
+
 import './App.css';
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
       <Header />
       <div className='game-container'>
         <Figure />
+        <WrongLetters />
       </div>
     </React.Fragment>
   );
