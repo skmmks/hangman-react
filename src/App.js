038f30126b1a1edc5f57f2e409437544b5da476b
Steve Min <stevekmin@gmail.com>
2020-11-23 09:09:20 -0800

    pass letter props from app to word component

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
       <div className='game-container'>
         <Figure />
         <WrongLetters />
-        <Word />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
     </React.Fragment>
   );
