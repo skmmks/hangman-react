5c24d1cc4e75130ebca45204ec30b2f43850d6bb
Steve Min <stevekmin@gmail.com>
2020-11-24 07:50:31 -0800

    add eventListenter to window object in app component

--- src/App.js
+++ src/App.js
@@ -12,6 +12,32 @@ let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 let playable = true;
 
+window.addEventListener('keydown', (e) => {
+  if (playable) {
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+      const letter = e.key.toLowerCase();
+
+      if (selectedWord.includes(letter)) {
+        if (!correctLetters.includes(letter)) {
+          correctLetters.push(letter);
+
+          displayWord();
+        } else {
+          showNotification();
+        }
+      } else {
+        if (!wrongLetters.includes(letter)) {
+          wrongLetters.push(letter);
+
+          updateWrongLettersEl();
+        } else {
+          showNotification();
+        }
+      }
+    }
+  }
+});
+
 function App() {
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
