a2654827265611f0546e10723b44a4fce7ead757
Steve Min <stevekmin@gmail.com>
2020-11-27 08:53:35 -0800

    set showNotification usestate

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
 
   useEffect(() => {
     const handleKeydown = (event) => {
